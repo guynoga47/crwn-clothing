6ab5474bb3022c3700e05b705ec7f930c99e81ee
Guy Noga <guynoga47@gmail.com>
2020-04-19 21:06:12 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart/cart.action.js
+++ src/components/cart/cart.action.js
@@ -2,5 +2,9 @@ import { CartActionTypes } from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
-  action: null,
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
 });
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import { addItem } from "../cart/cart.action";
+import { connect } from "react-redux";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+//prettier-ignore
+const CollectionItem = ({item, onAddItem}) => (
   <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+    {console.log(item, item.imageUrl, item.price)}
+    <div className="image" style={{ backgroundImage: `url(${item.imageUrl})` }} />
     <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      <span className="name">{item.name}</span>
+      <span className="price">{item.price}</span>
     </div>
+    <CustomButton onClick={()=>onAddItem(item)} inverted>
+      {" "}
+      Add to cart{" "}
+    </CustomButton>
   </div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  onAddItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
