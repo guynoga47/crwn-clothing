2a29c43002361ee5a225780db23b07f092dd00d8
Guy Noga <guynoga47@gmail.com>
2020-04-22 12:53:21 +0300

    added remove item and add item functionality to checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
+import {
+  clearItemFromCart,
+  removeItem,
+  addItem,
+} from "../../redux/cart/cart.action";
 import "./checkout-item.styles.scss";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CheckoutItem = (props) => (
   <div className="checkout-item">
@@ -7,10 +14,30 @@ const CheckoutItem = (props) => (
       <img src={props.cartItem.imageUrl} alt="item" />
     </div>
     <span className="name">{props.cartItem.name}</span>
-    <span className="quantity">{props.cartItem.quantity}</span>
+    <span className="quantity">
+      <div className="arrow" onClick={() => props.onRemoveItem(props.cartItem)}>
+        &#10094;
+      </div>
+      <span className="value">{props.cartItem.quantity}</span>
+      <div className="arrow" onClick={() => props.onAddItem(props.cartItem)}>
+        &#10095;
+      </div>
+    </span>
     <span className="price">{props.cartItem.price}</span>
-    <div className="remove-button">&#10005;</div>
+    <div
+      className="remove-button"
+      onClick={() => props.onClearFromCartButtonClick(props.cartItem)}
+    >
+      &#10005;
+    </div>
   </div>
 );
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  onClearFromCartButtonClick: (cartItem) =>
+    dispatch(clearItemFromCart(cartItem)),
+  onRemoveItem: (cartItem) => dispatch(removeItem(cartItem)),
+  onAddItem: (cartItem) => dispatch(addItem(cartItem)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,15 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   }
 };
+
+export const removeItem = (cartItems, cartItemToRemove) => {
+  if (cartItemToRemove.quantity > 1) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  } else {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+};
